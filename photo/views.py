c9cd0f0e89514d2dcd0c9aa6096c4176605284cc
sblee <sblee@synergyai.co>
2021-04-19 11:41:14 +0900

    [Add] Template & URL connect

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Photo
 
 # Create your views here.
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_create'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form': form})
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_update'
+    success_url = '/'
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+class PhotoDetail(DetailView):
+    model = Photo
+    template_name_suffix = '_detail'
