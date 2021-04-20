19f8e8ab298723e2fa427eb77bead56f3ba3a0af
sblee <sblee@synergyai.co>
2021-04-20 16:08:55 +0900

    [Add] Like Button

--- photo/urls.py
+++ photo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PhotoList, PhotoDelete, PhotoDetail, PhotoUpdate, PhotoCreate
+from .views import PhotoList, PhotoDelete, PhotoDetail, PhotoUpdate, PhotoCreate, PhotoLike, Photofavorite
 
 app_name = "photo"
 urlpatterns = [
     path("create/", PhotoCreate.as_view(), name='create'),
+    path("like/<int:photo_id>/", PhotoLike.as_view(), name='like'),
+    path("favorite/<int:photo_id>/", Photofavorite.as_view(), name='favorite'),
     path("delete/<int:pk>/", PhotoDelete.as_view(), name='delete'),
     path("update/<int:pk>/", PhotoUpdate.as_view(), name='update'),
     path("detail/<int:pk>/", PhotoDetail.as_view(), name='detail'),
--- photo/views.py
+++ photo/views.py
@@ -55,3 +55,40 @@ class PhotoDelete(DeleteView):
 class PhotoDetail(DetailView):
     model = Photo
     template_name_suffix = '_detail'
+
+
+from django.views.generic.base import View
+from django.http import HttpResponseForbidden
+from urllib.parse import urlparse
+
+class PhotoLike(View):
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        else:
+            if 'photo_id' in kwargs:
+                photo_id = kwargs['photo_id']
+                photo = Photo.objects.get(pk=photo_id)
+                user = request.user
+                if user in photo.like.all():
+                    photo.like.remove(user)
+                else:
+                    photo.like.add(user)
+            referer_url = request.META.get('HTTP_REFERER')
+            path = urlparse(referer_url).path
+            return HttpResponseRedirect(path)
+
+class Photofavorite(View):
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        else:
+            if 'photo_id' in kwargs:
+                photo_id = kwargs['photo_id']
+                photo = Photo.objects.get(pk=photo_id)
+                user = request.user
+                if user in photo.favorite.all():
+                    photo.favorite.remove(user)
+                else:
+                    photo.favorite.add(user)
+            return HttpResponseRedirect('/')
